dbb66fb1fa7b558740d934a501be23d6e47d14f9
Abhash Jha <51852016+abhash-er@users.noreply.github.com>
2019-09-02 21:07:39 +0530

    Update aes3.py

--- Cryptography/Hakuna_Matata/aes3.py
+++ Cryptography/Hakuna_Matata/aes3.py
@@ -1,13 +1,13 @@
 from Crypto.Cipher import AES
 import binascii, sys
 
-KEY="9aF738g9AkI112#g"
-IV="The message is p"
+KEY="8cG926f6#anonCTF"
+IV="The message is e"
 
-cipher1="9e128e7bc9ab9cc9d8b13ec77389436a"
+cipher1="33e616929ba5a6ee8c8974338fbbf12e"
 
 def decrypt(cipher,passphrase):
     aes = AES.new(passphrase,AES.MODE_CBC,IV)
     return aes.decrypt(cipher)
 
-print "decrypted data: " + binascii.hexlify(decrypt(binascii.unhexlify(cipher1), KEY))
+print "decrypted data: " + decrypt(binascii.unhexlify(cipher1), KEY)
